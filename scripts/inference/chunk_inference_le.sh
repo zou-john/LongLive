06f8156caa24e49a48c455d5d161affe770fef8d
zou-john <john_zou@brown.edu>
2026-01-31 13:53:37 -0500

    added chunk infernece

--- scripts/inference/chunk_inference_le.sh
+++ scripts/inference/chunk_inference_le.sh
@@ -2,4 +2,4 @@ torchrun \
   --nproc_per_node=1 \
   --master_port=29500 \
   chunk_inference.py \
-  --config_path configs/longlive_streaming_inference.yaml
+  --config_path configs/longlive_streaming_inference_le.yaml
